680d5eb70eda13f29ae158b5e8bbeacd9c41f032
ssodhi26 <shauryasodhi@gmail.com>
2024-01-22 11:53:04 -0800

    prod fixes #2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
-import {
-  MatBottomSheet,
-  MatBottomSheetRef,
-} from '@angular/material/bottom-sheet';
-import { ContactComponent } from '../contact/contact.component';
+
 
 @Component({
   selector: 'app-home',
@@ -16,13 +12,11 @@ export class HomeComponent {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private _bottomSheet: MatBottomSheet
+    
   ){
     this.router.navigate(['intro']);
   }
 
-  openBottomSheet(): void {
-    this._bottomSheet.open(ContactComponent);
-  }
+  
 
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,ViewEncapsulation } from '@angular/core';
+import {
+  MatBottomSheet,
+  MatBottomSheetRef,
+} from '@angular/material/bottom-sheet';
+import { ContactComponent } from '../contact/contact.component';
 
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class SkillsComponent {
+
+  constructor(
+    private _bottomSheet: MatBottomSheet
+  ){}
+
+  openBottomSheet(): void {
+    this._bottomSheet.open(ContactComponent);
+  }
 
 }
